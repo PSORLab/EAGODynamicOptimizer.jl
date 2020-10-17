35c51478e04fc679f1f7edf8bd25ba62ba92254a
Matt W <matthew.wilhelm@uconn.edu>
2020-10-16 21:37:50 -0400

    Save work on lower problem

--- src/subproblems.jl
+++ src/subproblems.jl
@@ -50,15 +50,73 @@ function presolve_global!(t::DynamicExt, m::EAGO.Optimizer)
     return
 end
 
+function supports_affine_relaxation(integrator)
+    supports(integrator, Relaxation{Lower}()) &&
+    supports(integrator, Relaxation{Upper}()) &&
+    supports(integrator, Subgradient{Lower}()) &&
+    supports(integrator, Subgradient{Upper}())
+end
+
 function lower_problem!(t::DynamicExt, opt::EAGO.Optimizer)
 
+    integrator = t.integrator
+    np = t.np
+    supports_affine = supports_affine_relaxation(integrator)
+
+    # reset box used to evaluate relaxation
     n = opt._current_node
+    lvbs = n.lower_variable_bounds
+    uvbs = n.upper_variable_bounds
+    setall!(integrator, ParameterBound{Lower}(), lvbs)
+    setall!(integrator, ParameterBound{Upper}(), uvbs)
 
-    # TODO: calculate
+    # set reference point to evaluate relaxation
+    if supports_affine
+       @__dot__ m._current_xref = 0.5*(lvbs + uvbs)
+       setall!(integrator, ParameterValue(), m._current_xref)
+       @__dot__ t.p_intv = Interval(lvbs, uvbs)
+       @__dot__ t.p_mc = MC{N,NS}(m._current_xref, t.p_intv, 1:np)
+   end
 
-    opt._lower_objective_value = relax_problem_value
-    opt._lower_solution = relax_problem_solution
-    opt._lower_feasibility = relaxed_problem_feasibility
+    # relaxes pODE
+    relax!(integator)
+
+    # unpacks bounds, relaxations, and subgradients at specific points
+    # and computes objective bound/relaxation...
+    getall!(t.lo, integrator, Bound{Lower}())
+    getall!(t.hi, integrator, Bound{Upper}())
+    @__dot__ t.x_intv = Interval(t.lo, t.hi)
+
+    if supports_affine
+        getall!(t.cv, integrator, Relaxation{Lower}())
+        getall!(t.cc, integrator, Relaxation{Upper}())
+        getall!(t.cv_grad, integrator, Subgradient{Lower}())
+        getall!(t.cc_grad, integrator, Subgradient{Upper}())
+        @__dot__ t.x_mc = MC{N,NS}(t.cv, t.cc, t.intv, t.cv_grad, t.cc_grad, false)
+        t.obj_mc = t.objective(t.x_mc, t.p_mc)
+
+        # add affine relaxation... to opt problem
+
+    else
+        t.obj_intv = t.objective(t.x_intv, t.p_intv)
+    end
+
+    if supports_affine
+        if valid_flag
+            opt._lower_objective_value = MOI.get(relaxed_optimizer, MOI.ObjectiveValue())
+            for i = 1:m._working_problem._variable_count
+                opt._lower_solution[i] = MOI.get(relaxed_optimizer, MOI.VariablePrimal(), opt._relaxed_variable_index[i])
+            end
+            opt._lower_feasibility = supports_affine ? : true
+        else
+        end
+    else
+        opt._lower_objective_value = t.obj_intv.lo
+        opt._lower_solution = m._current_xref
+        opt._lower_feasibility = true
+    end
+
+    return nothing
 end
 
 function upper_problem!(t::DynamicExt, opt::EAGO.Optimizer)
