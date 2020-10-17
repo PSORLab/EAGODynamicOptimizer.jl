b458a94243e92eb5397fff6fb06fe7ec4d124c59
Matt W <matthew.wilhelm@uconn.edu>
2020-10-17 17:00:33 -0400

    Work on load trajectories

--- src/state_vector.jl
+++ src/state_vector.jl
@@ -31,15 +31,37 @@ function setindex!(d::Trajectory{T}, val::T, i::Int64, j::Int64) where T<:Number
     return nothing
 end
 
+function extract_static(::Type{Val{N}}, grad::Vector{Matrix{Float64}}, i::Int64, j::Int64)
+    return SVector{N,Float64}()
+end
+
 function load_trajectory!(d::Trajectory{MC{N,T}}, cv::Matrix{Float64}, cc::Matrix{Float64},
                           l::Matrix{Float64}, u::Matrix{Float64}, cv_grad::Vector{Matrix{Float64}},
                           cc_grad::Vector{Matrix{Float64}}) where {N,T<:RelaxTag}
+    for i = 1:d.nt
+        for j = 1:d.nx
+            cvg = extract_static(Val{N}, cc_grad, i, j)
+            ccg = extract_static(Val{N}, cc_grad, i, j)
+            d.v[i][j] = MC{N,T}(cv[j,i], cc[j,i], Interval(xL[j,i], xU[j,i])), cvg, ccg, false)
+        end
+    end
     return nothing
 end
+
 function load_trajectory!(d::Trajectory{Interval{Float64}}, xL::Matrix{Float64}, xU::Matrix{Float64})
+    for i = 1:d.nt
+        for j = 1:d.nx
+            d.v[i][j] = Interval(xL[j,i], xU[j,i])
+        end
+    end
     return nothing
 end
 
 function load_trajectory!(d::Trajectory{Float64}, x::Matrix{Float64})
+    for i = 1:d.nt
+        for j = 1:d.nx
+            d.v[i][j] = x[j,i]
+        end
+    end
     return nothing
 end
