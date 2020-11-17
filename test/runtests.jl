94008845e7b08dff7c331554c4a26a154317066c
Matt W <matthew.wilhelm@uconn.edu>
2020-11-17 00:36:11 -0500

    Fix starting unit tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using Test, EAGODynamicOptimizer
 
 @testset "State Vector" begin
-    traj = Trajectory{Float64}(5, 2)
+    traj = EAGODynamicOptimizer.Trajectory{Float64}(5, 2)
     @test length(traj.v[1]) == 2
     @test length(traj.v) == 5
 end
