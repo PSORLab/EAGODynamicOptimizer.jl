6aaea4e049a520be20813a466599d0f35c666ed4
Matt W <matthew.wilhelm@uconn.edu>
2021-02-09 02:25:02 -0500

    SIP extension design complete (to debug)

--- src/semiinfinite/sip_extension.jl
+++ src/semiinfinite/sip_extension.jl
@@ -1,68 +1,74 @@
-function build_model(t::DynamicExt, a::A, s::S, p::SIPProblem) where {A <: AbstractSIPAlgo,
-                                                                      S <: AbstractSubproblemType}
-    model = Model(EAGO.Optimizer)
-    for (k,v) in get_sip_kwargs(a,s,p)
+mutable struct SIPDynamicExt <: EAGO.ExtensionType
+    llp_ext::DynamicExt
+    bnd_ext::DynamicExt
+end
+function get_ext(m::SIPDynamicExt, s::T) where T <: Union{LowerLevel1,LowerLevel2,LowerLevel3}
+    get_ext.llp
+end
+function get_ext(m::SIPDynamicExt, s::T) where T <: Union{LowerProblem,UpperProblem,ResProblem}
+    get_ext.bnd_ext
+end
+
+function build_model(t::SIPDynamicExt, a::A, s::S, p::SIPProblem) where {A <: AbstractSIPAlgo, S <: AbstractSubproblemType}
+    model, v = EAGODynamicModel(get_ext(t,s), EAGO.Optimizer)
+    for (k,v) in get_sip_kwargs(s,p)
         MOI.set(model, MOI.RawParameter(String(k)), v)
     end
-    vL, vU, nv = get_bnds(s,a,p)
     @variable(model, vL[i] <= v[i=1:nv] <= vU[i])
     return model, v
 end
 
-function sip_llp!(t::DynamicExt, alg::A, s::S, result::SIPResult,
+function sip_llp!(t::SIPDynamicExt, alg::A, s::S, result::SIPResult,
                   sr::SIPSubResult, prob::SIPProblem, cb::SIPCallback,
-                  i::Int64, tol::Float64 = -Inf) where {A <: AbstractSIPAlgo,
-                                                        S <: AbstractSubproblemType}
-
-    # build the model
+                  i::Int64, tol::Float64 = -Inf)
     m, p = build_model(t, alg, s, prob)
-    EAGO.set_tolerance!(t, alg, s, m, sr, i)
+    set_tolerance!(t, alg, s, m, sr, i)
 
     # define the objective
-    g(p...) = cb.gSIP[i](xbar, p)
-    add_supported_objective!(m, g)
+    xbar = get_xbar(t, alg, s, sr)
+    add_supported_objective!(m, (y, p) -> -cb.gSIP[i](y, xbar, p))
 
     # optimize model and check status
     JuMP.optimize!(m)
     tstatus = JuMP.termination_status(m)
     rstatus = JuMP.primal_status(m)
-    feas = EAGO.llp_check(prob.local_solver, tstatus, rstatus)
+    feas = llp_check(prob.local_solver, tstatus, rstatus)
 
     # fill buffer with subproblem result info
-    load!(s, buffer, feas, -JuMP.objective_bound(m), JuMP.value(x))
+    psol = JuMP.value.(p)
+    load!(s, sr, feas, -JuMP.objective_value(m), -JuMP.objective_bound(m), psol)
     result.solution_time += MOI.get(m, MOI.SolveTime())
 
     return nothing
 end
 
-function sip_bnd!(t::DynamicExt, alg::A, s::S, sr::SIPSubResult, result::SIPResult,
+function sip_bnd!(t::SIPDynamicExt, alg::A, s::S, sr::SIPSubResult, result::SIPResult,
                   prob::SIPProblem, cb::SIPCallback) where {A <: AbstractSIPAlgo,
                                                             S <: AbstractSubproblemType}
 
     # create JuMP model
     m, x = build_model(t, alg, s, prob)
-    disc_set = get_disc_set(s, prob)
 
     for i = 1:prob.nSIP
         ϵ_g = get_eps(s, sr, i)
+        disc_set = get_disc_set(t, alg, s, sr, i)
         for j = 1:length(disc_set)
-            g = (x...) -> cb.gSIP[i](x, disc_set[j][i]) + ϵ_g
-            add_supported_constraint!(m, g)
+            pbar = disc_set[j]
+            add_supported_constraint!(m, (y, x) -> cb.gSIP[i](y, x, pbar) + ϵ_g)
         end
     end
 
     # define the objective
-    obj = (x...) -> cb.f(x)
-    add_supported_objective!(m, obj)
+    add_supported_objective!(m, (y, x) -> cb.f(y, x))
 
     # optimize model and check status
     JuMP.optimize!(m)
     t_status = JuMP.termination_status(m)
     r_status = JuMP.primal_status(m)
-    feas = bnd_check(prob.local_solver, t_status, r_status, eps_g)
+    feas = bnd_check(prob.local_solver, t_status, r_status)
 
     # fill buffer with subproblem result info
-    load!(s, buffer, feas, JuMP.objective_bound(m), JuMP.value(x))
+    load!(s, sr, feas, JuMP.objective_value(m), JuMP.objective_bound(m), JuMP.value.(x))
     result.solution_time += MOI.get(m, MOI.SolveTime())
 
     return nothing
